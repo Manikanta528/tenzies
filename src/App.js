a55378813dcf47fab5d6ae34add90ac3e2b542ca
Manikanta528 <chmanikanta528@gmail.com>
2022-12-05 20:26:42 +0530

    roll dice added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from "react";
 import Die from "./Components/Die";
+import { nanoid } from "nanoid";
 
 export default function App(){
+    const [dices, setDices] = React.useState(allNewDice());
+
+    function allNewDice(){
+        let arr = [];
+    
+        for(let i = 0 ; i < 10 ; i++){
+            let obj = {
+                value : Math.floor(Math.random() * 6) + 1,
+                isHeld : false,
+                id : nanoid()
+            };
+            arr.push(obj);
+        }
+        return arr;
+    }
+
+    function rollHandle(){
+        setDices(allNewDice());
+    }
+    
+    const all10Diecs = dices.map(die => <Die key={die.id} value={die.value} />)
+
     return(
         <main>
             <div className="die-container">
-                <Die value="1"/>
-                <Die value="4"/>
-                <Die value="2"/>
-                <Die value="3"/>
-                <Die value="6"/>
-                <Die value="2"/>
-                <Die value="5"/>
-                <Die value="4"/>
-                <Die value="3"/>
-                <Die value="1"/>
-            </div>
-            
+                {all10Diecs}
+            </div>   
+            <button className="roll-button" onClick={()=>rollHandle()}>Roll</button>
         </main>
     )
 }
+
